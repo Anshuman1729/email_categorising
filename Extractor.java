4146ddace93b98b48b7b22bcd90e70ccbc1d8fbe
Anshuman Khare <103522278+Anshuman1729@users.noreply.github.com>
2026-02-09 23:17:48 +0530

    Update Extractor.java

--- Extractor.java
+++ Extractor.java
@@ -8,27 +8,17 @@ public class Extractor {
 
     public static void main(String[] args) throws Exception{
 
-
-        /**
-         * Add the tokenizer training model
-         * initialize the tokenizer model to the input training model
-         */
         InputStream in = new FileInputStream("models/en-token.bin");
         TokenizerModel mo = new TokenizerModel(in);
 
-        //Creating a tokenizer object
         TokenizerME tokenize = new TokenizerME(mo);
 
-        //Email text
-//        String email = "Hi there this is the email text.";
         BufferedReader read = new BufferedReader(new FileReader("Emails.txt"));
         String text;
         while ((text = read.readLine()) != null){
 
-            //Tokenize the email
             String[] tokens = tokenize.tokenize(text);
 
-            //Print the tokens
             for(String token:tokens){
                 System.out.print(token + ", ");
             }
@@ -39,3 +29,4 @@ public class Extractor {
     }
 
 }
+
